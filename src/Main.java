0ba8110d483d2465425f4a23c393f1f021c7927e
molochko <bel.sneshka@gmail.com>
2023-10-26 21:16:05 +0500

    Отформатирован код

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
-public class Main
-{
+public class Main {
     public static void main(String[] args) {
         int x = 1753;
         System.out.println("Стоимость билета:" + x);
         int y = 20;
-        int m = (int)(x / y);
-        System.out.println("Начисленные мили: " + m + " миль" );
+        int m = (int) (x / y);
+        System.out.println("Начисленные мили: " + m + " миль");
     }
 }

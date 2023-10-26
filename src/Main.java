bffe7f2839d9388acf3043158e1eaec1d8c5bc5d
molochko <bel.sneshka@gmail.com>
2023-10-26 21:01:13 +0500

    Убраны сканеры,добавлен игнор

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
-import java.util.Scanner;
 public class Main
 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введите стоимость билета:");
-        int StoimostBileta = in.nextInt();
-        int MilyaZaRubli = 20;
-        int Mili = (int)(StoimostBileta / MilyaZaRubli);
-        System.out.println("Начисленные мили: " + Mili + " миль" );
-        in.close();
+        int x = 1753;
+        System.out.println("Стоимость билета:" + x);
+        int y = 20;
+        int m = (int)(x / y);
+        System.out.println("Начисленные мили: " + m + " миль" );
     }
 }
